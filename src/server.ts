ec3854d1a6df73a30e5c40f4e0a79b00c1c6feb7
Ralfas Ragauskas <ralfasr02@gmail.com>
2026-09-25 11:01:53 +0200

    Format Elasticsearch results in Search API

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,23 @@ app.get("/search",  async (req, res) => {
     size: 5
   });
 
-  res.json(response.hits.hits);
+  res.json(
+    response.hits.hits.map(hit =>{
+        const source = hit._source as {
+            title?: string;
+            url?: string;
+            body?: string;
+            headings?: string[];
+            url_path_dir3?: string;
+        };
+        return {
+            title: source.title,
+            url: source.url,
+            version: source.url_path_dir3,
+            snippet: source.body?.substring(0,200)
+        };
+    })
+);
 });
 
 app.listen(3000, () => {
