00e906f928721ebfa5cc336b483559f5ec061cf8
Ralfas Ragauskas <ralfasr02@gmail.com>
2026-09-25 10:40:39 +0200

    Build initial Elasticsearch Search API

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,27 @@
+import { Client } from "@elastic/elasticsearch"
 import express from "express";
+
+const client = new Client({
+  node: "http://localhost:9200"
+});
 
 const app = express();
 
-app.get("/search", (req, res) => {
-  res.json({ message: "Search API works" });
+app.get("/search",  async (req, res) => {
+  const query = req.query.q;
+
+  const response = await client.search({
+    index: "medcom-crawler-test",
+    query: {
+        multi_match: {
+            query: query as string,
+            fields: ["title", "body"]
+        }
+    },
+    size: 5
+  });
+
+  res.json(response.hits.hits);
 });
 
 app.listen(3000, () => {
